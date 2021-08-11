7af4355be7cb8f9192fc0e0640d008ef0c4450f8
Toothyca <Toothyca@gmail.com>
2021-08-10 21:55:39 -0700

    new multi-event generation
    can now generate and print events with multiple time ranges

--- generate_sample_times.py
+++ generate_sample_times.py
@@ -11,21 +11,39 @@ DURATION = random.randint(900,3600) # 15-60 minutes
 
 # Populate the eventList with events using random days and times
 for i in range(0,TOTAL_EVENTS):
-    days = [0,1,2,3,4,5,6]
-    a = datetime.time(random.randint(0,23),random.randint(0,59),random.randint(0,59)) # 0-23 hours, 0-60 min, 0-60 sec
-    for j in range(0,random.randint(0,6)):
-        target = random.randint(0,6)
-        if target in days:
-            days.remove(target)
-    event = Event('{0}{1}'.format(eventNames[random.randint(0, len(eventNames) - 1)], random.randint(50, 300)), days, a, DURATION, 2021, abbreviateDate=random.randint(0,3))
-    eventList.append(event)
+    # Generate a list of 1-5 events
+    multEvent = []
+    for j in range(0, random.randint(1, 5)):
+        days = [0,1,2,3,4,5,6]
+        a = datetime.time(random.randint(0,23),random.randint(0,59),random.randint(0,59)) # 0-23 hours, 0-60 min, 0-60 sec
+        for k in range(0,random.randint(0,6)):
+            target = random.randint(0,6)
+            if target in days:
+                days.remove(target)
+        event = Event('{0}{1}'.format(eventNames[random.randint(0, len(eventNames) - 1)], random.randint(50, 300)), days, a, DURATION, 2021, abbreviateDate=random.randint(0,3))
+        multEvent.append(event)
+    eventList.append(multEvent)
 
-for event in eventList:
+# Print multiple time ranges on the same line with the same template
+for multEvent in eventList:
     for template in templates:
-        print(template.format(
-            name=event.name,
-            days=event.days_str,
-            startTime=event.startTime,
-            endTime=event.endTime,
-            duration=event.duration,
-            year=event.year), end="\n")
+        # Randomize between separating multiple time ranges with ";" or "and"
+        if(random.randint(0, 1)):
+            delimiter = "; "
+        else:
+            delimiter = " and "
+
+        # If printing the last time range in the event, end with a new line, otherwise, the randomly decided delimiter
+        for count, event in enumerate(multEvent):           
+            if(count == len(multEvent)-1):
+                ending = "\n"
+            else:
+                ending = delimiter
+                
+            print(template.format(
+                name=event.name,
+                days=event.days_str,
+                startTime=event.startTime,
+                endTime=event.endTime,
+                duration=event.duration,
+                year=event.year), end=ending)
