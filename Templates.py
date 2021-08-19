e0e9c907938463a286fa599c95366ed9f12a3055
Justin Lee <justin@atomic.local>
2021-08-19 15:07:25 -0700

    Added new todo note

--- Templates.py
+++ Templates.py
@@ -1,3 +1,5 @@
+# TODO: Handle abbreviations and delimeters here, you might find the link below useful!
+# https://realpython.com/python-f-strings/#f-strings-a-new-and-improved-way-to-format-strings-in-python
 templates = [
     "{eventName} - {days} from {startTime} to {endTime}",
     "I will be in {eventName} on {days} from {startTime} to {endTime}",
